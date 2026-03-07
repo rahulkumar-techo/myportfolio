e3eea8cb10a6eb366b768efd85ae709df99ac927
Rahul Kumar <rahulkumar7250883289@gmail.com>
2026-03-07 19:06:32 +0530

    added:CI/CD

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import { dirname } from "node:path"
-import { fileURLToPath } from "node:url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals"
+import nextTypescript from "eslint-config-next/typescript"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-})
-
-export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript")
-]
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "coverage/**",
+    "node_modules/**"
+  ])
+])
